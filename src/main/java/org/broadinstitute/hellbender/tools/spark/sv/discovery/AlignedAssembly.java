2ae3fa4c844e0f9a7bf9659c8dc74431a5a51604
vruano <valentin.ruano@gmail.com>
2017-08-04 11:02:49 -0400

    Now I produced association between contigs and ref vs alt plus qualities of the a association

--- src/main/java/org/broadinstitute/hellbender/tools/spark/sv/discovery/AlignedAssembly.java
+++ src/main/java/org/broadinstitute/hellbender/tools/spark/sv/discovery/AlignedAssembly.java
@@ -5,9 +5,19 @@ import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
 import com.google.common.annotations.VisibleForTesting;
+import htsjdk.samtools.*;
+import org.broadinstitute.hellbender.tools.spark.sv.SVConstants;
+import org.broadinstitute.hellbender.utils.SimpleInterval;
+import org.broadinstitute.hellbender.utils.Utils;
+import org.broadinstitute.hellbender.utils.bwa.BwaMemAligner;
+import org.broadinstitute.hellbender.utils.bwa.BwaMemAlignment;
+import org.broadinstitute.hellbender.utils.read.CigarUtils;
+import org.broadinstitute.hellbender.utils.read.GATKRead;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.IntFunction;
 
 /**
  * Holding necessary information about a local assembly for use in SV discovery.
@@ -18,6 +28,191 @@ public final class AlignedAssembly {
     public final int assemblyId;
 
     public final List<AlignedContig> alignedContigs;
+
+    /**
+     * Each assembled contig should have at least one such accompanying structure, or 0 when it is unmapped.
+     */
+    @DefaultSerializer(AlignmentInterval.Serializer.class)
+    static public final class AlignmentInterval {
+
+        public static final int MISSING_AS = -1;
+        public final SimpleInterval referenceInterval;
+        public final int startInAssembledContig;   // 1-based, inclusive
+        public final int endInAssembledContig;     // 1-based, inclusive
+
+        public final Cigar cigarAlong5to3DirectionOfContig;
+
+        public final boolean forwardStrand;
+        public final int mapQual;
+        public final int mismatches;
+        public final int alignmentScore;
+
+        @VisibleForTesting
+        public AlignmentInterval(final SAMRecord samRecord) {
+
+            final boolean isMappedReverse = samRecord.getReadNegativeStrandFlag();
+            this.referenceInterval = new SimpleInterval(samRecord);
+            this.startInAssembledContig = getAlignmentStartInOriginalContig(samRecord);
+            this.endInAssembledContig = getAlignmentEndInOriginalContig(samRecord);
+
+            this.cigarAlong5to3DirectionOfContig = isMappedReverse ? CigarUtils.invertCigar(samRecord.getCigar()) : samRecord.getCigar();
+            this.forwardStrand = !isMappedReverse;
+            this.mapQual = samRecord.getMappingQuality();
+            final Integer nMismatches = samRecord.getIntegerAttribute("NM");
+            this.mismatches = nMismatches==null ? SVConstants.DiscoveryStepConstants.MISSING_NM : nMismatches;
+            this.alignmentScore = Optional.of(samRecord.getIntegerAttribute(SAMTag.AS.name())).orElse(MISSING_AS);
+        }
+
+        public AlignmentInterval(final GATKRead read) {
+            final boolean isMappedReverse = read.isReverseStrand();
+            this.referenceInterval = new SimpleInterval(read);
+            this.startInAssembledContig = read.getFirstAlignedReadPosition();
+            this.endInAssembledContig = read.getLastAlignedReadPosition();
+
+            this.cigarAlong5to3DirectionOfContig = isMappedReverse ? CigarUtils.invertCigar(read.getCigar()) : read.getCigar();
+            this.forwardStrand = !isMappedReverse;
+            this.mapQual = read.getMappingQuality();
+            final Integer nMismatches = read.getAttributeAsInteger(SAMTag.NM.name());
+            this.mismatches = nMismatches==null ? SVConstants.DiscoveryStepConstants.MISSING_NM : nMismatches;
+            this.alignmentScore = Optional.of(read.getAttributeAsInteger(SAMTag.AS.name())).orElse(MISSING_AS);
+        }
+
+        @VisibleForTesting
+        public AlignmentInterval(final BwaMemAlignment alignment, final List<String> refNames, final int unclippedContigLength) {
+            this(Utils.nonNull(alignment, "the input alignment cannot be null"), refNames::get, unclippedContigLength);
+        }
+
+        public AlignmentInterval(final BwaMemAlignment alignment, final SAMSequenceDictionary dictionary, final int unclippsedContigLength) {
+            this(Utils.nonNull(alignment, "the input alignment cannot be null"), i -> {
+
+                if (dictionary == null) { throw new IllegalArgumentException("bad dictionary"); }
+                final SAMSequenceRecord sequence = dictionary.getSequence(i);
+                if (sequence == null) {
+                    throw new IllegalStateException("alignment index " + alignment.getRefId() + " out of bounds " + dictionary.getSequences().size());
+                }
+                return sequence.getSequenceName();
+            }, unclippsedContigLength);
+        }
+
+        public AlignmentInterval(final BwaMemAlignment alignment, final IntFunction<String> refNames, final int unclippedContigLength) {
+            this.referenceInterval = new SimpleInterval(refNames.apply(alignment.getRefId()), alignment.getRefStart()+1, alignment.getRefEnd()); // +1 because the BwaMemAlignment class has 0-based coordinate system
+            this.forwardStrand = (alignment.getSamFlag()& SAMFlag.READ_REVERSE_STRAND.intValue())==0;
+            this.cigarAlong5to3DirectionOfContig = forwardStrand ? TextCigarCodec.decode(alignment.getCigar()) : CigarUtils.invertCigar(TextCigarCodec.decode(alignment.getCigar()));
+            Utils.validateArg(cigarAlong5to3DirectionOfContig.getReadLength() + SVVariantDiscoveryUtils.getTotalHardClipping(cigarAlong5to3DirectionOfContig)
+                    == unclippedContigLength, "contig length provided in constructor and inferred length by computation are different: " + unclippedContigLength + "\t" + alignment.getCigar().toString());
+
+            this.mapQual = Math.max(SAMRecord.NO_MAPPING_QUALITY, alignment.getMapQual()); // BwaMemAlignment has negative mapQ for unmapped sequences, not the same as its SAMRecord conversion (see BwaMemAlignmentUtils.applyAlignment())
+            this.mismatches = alignment.getNMismatches();
+            this.alignmentScore = alignment.getAlignerScore();
+            if ( forwardStrand ) {
+                this.startInAssembledContig = alignment.getSeqStart() + 1;
+                this.endInAssembledContig = alignment.getSeqEnd();
+            } else {
+                this.startInAssembledContig = unclippedContigLength - alignment.getSeqEnd() + 1;
+                this.endInAssembledContig = unclippedContigLength - alignment.getSeqStart();
+            }
+        }
+
+        public AlignmentInterval(final SimpleInterval referenceInterval, final int startInAssembledContig, final int endInAssembledContig,
+                                 final Cigar cigarAlong5to3DirectionOfContig, final boolean forwardStrand, final int mapQual, final int mismatches, final int alignmentScore) {
+            this.referenceInterval = referenceInterval;
+            this.startInAssembledContig = startInAssembledContig;
+            this.endInAssembledContig = endInAssembledContig;
+
+            this.cigarAlong5to3DirectionOfContig = cigarAlong5to3DirectionOfContig;
+
+            this.forwardStrand = forwardStrand;
+            this.mapQual = mapQual;
+            this.mismatches = mismatches;
+            this.alignmentScore = alignmentScore;
+        }
+
+        static int getAlignmentStartInOriginalContig(final SAMRecord samRecord) {
+            return SVVariantDiscoveryUtils.getNumClippedBases(!samRecord.getReadNegativeStrandFlag(), samRecord.getCigar()) + 1;
+        }
+
+        static int getAlignmentEndInOriginalContig(final SAMRecord samRecord) {
+            final Cigar cigar = samRecord.getCigar();
+            return cigar.getReadLength() + SVVariantDiscoveryUtils.getTotalHardClipping(cigar) - SVVariantDiscoveryUtils.getNumClippedBases(samRecord.getReadNegativeStrandFlag(), cigar);
+        }
+
+        public AlignmentInterval(final Kryo kryo, final Input input) {
+            final String chr = input.readString();
+            final int refStart = input.readInt(),
+                    refEnd = input.readInt();
+            referenceInterval = new SimpleInterval(chr, refStart, refEnd);
+            startInAssembledContig = input.readInt();
+            endInAssembledContig = input.readInt();
+            cigarAlong5to3DirectionOfContig = TextCigarCodec.decode(input.readString());
+            forwardStrand = input.readBoolean();
+            mapQual = input.readInt();
+            mismatches = input.readInt();
+            alignmentScore = input.readInt();
+        }
+
+        public void serialize(final Kryo kryo, final Output output) {
+            output.writeString(referenceInterval.getContig());
+            output.writeInt(referenceInterval.getStart());
+            output.writeInt(referenceInterval.getEnd());
+            output.writeInt(startInAssembledContig);
+            output.writeInt(endInAssembledContig);
+            output.writeString(TextCigarCodec.encode(cigarAlong5to3DirectionOfContig));
+            output.writeBoolean(forwardStrand);
+            output.writeInt(mapQual);
+            output.writeInt(mismatches);
+            output.writeInt(alignmentScore);
+        }
+
+        public static final class Serializer extends com.esotericsoftware.kryo.Serializer<AlignmentInterval> {
+            @Override
+            public void write( final Kryo kryo, final Output output, final AlignmentInterval alignmentInterval){
+                alignmentInterval.serialize(kryo, output);
+            }
+
+            @Override
+            public AlignmentInterval read(final Kryo kryo, final Input input, final Class<AlignmentInterval> clazz ) {
+                return new AlignmentInterval(kryo, input);
+            }
+        }
+
+        public static final String PACKED_STRING_REP_SEPARATOR = "_";
+        /**
+         * @return  A packed String representation of this alignment interval; intended for debugging or annotation usage (both requires compactified message).
+         */
+        public String toPackedString() {
+            return String.join(PACKED_STRING_REP_SEPARATOR, String.valueOf(startInAssembledContig), String.valueOf(endInAssembledContig),
+                    referenceInterval.toString(), (forwardStrand ? "+" : "-"),
+                    TextCigarCodec.encode(cigarAlong5to3DirectionOfContig), String.valueOf(mapQual), String.valueOf(mismatches));
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            AlignmentInterval that = (AlignmentInterval) o;
+
+            if (startInAssembledContig != that.startInAssembledContig) return false;
+            if (endInAssembledContig != that.endInAssembledContig) return false;
+            if (forwardStrand != that.forwardStrand) return false;
+            if (mapQual != that.mapQual) return false;
+            if (mismatches != that.mismatches) return false;
+            if (!referenceInterval.equals(that.referenceInterval)) return false;
+            return cigarAlong5to3DirectionOfContig.equals(that.cigarAlong5to3DirectionOfContig);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = referenceInterval.hashCode();
+            result = 31 * result + startInAssembledContig;
+            result = 31 * result + endInAssembledContig;
+            result = 31 * result + cigarAlong5to3DirectionOfContig.hashCode();
+            result = 31 * result + (forwardStrand ? 1 : 0);
+            result = 31 * result + mapQual;
+            result = 31 * result + mismatches;
+            return result;
+        }
+    }
 
     public AlignedAssembly(final int assemblyId, final List<AlignedContig> alignedContigs) {
         this.assemblyId = assemblyId;
--- src/test/java/org/broadinstitute/hellbender/tools/spark/sv/discovery/AlignedAssemblyUnitTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/spark/sv/discovery/AlignedAssemblyUnitTest.java
@@ -5,6 +5,7 @@ import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
 import htsjdk.samtools.Cigar;
+import htsjdk.samtools.SAMRecord;
 import htsjdk.samtools.TextCigarCodec;
 import org.broadinstitute.hellbender.tools.spark.sv.evidence.AlignedAssemblyOrExcuse;
 import org.broadinstitute.hellbender.utils.SimpleInterval;
@@ -25,6 +26,94 @@ public class AlignedAssemblyUnitTest extends BaseTest {
     private static final String dummyRefName = "1";
     private static final List<String> refNames = Collections.singletonList(dummyRefName);
 
+    /**
+     * These alignment records are supposed to be associated with the 4 possible types of evidence we could see for an inversion,
+     *   where the chr1:101-200 bases are inverted, namely
+     * INV55, where lower  contig coordinate is associated with a forward  strand lower reference coordinate, and
+     *              higher contig coordinate is associated with a negative strand higher reference/contig coordinate
+     * INV55, where lower  contig coordinate is associated with a forward  strand higher reference coordinate, and
+     *              higher contig coordinate is associated with a negative strand lower reference/contig coordinate
+     * INV33, where lower  contig coordinate is associated with a negative strand lower reference coordinate, and
+     *              higher contig coordinate is associated with a forward  strand higher reference/contig coordinate
+     * INV33, where lower  contig coordinate is associated with a forward  strand higher reference coordinate, and
+     *              higher contig coordinate is associated with a negative strand lower reference/contig coordinate
+     * Finally, one must be aware of the fact that BWA always outputs CIGAR with a '+'-strand representation,
+     *   therefore we must use such in constructing the BwaMemAlignment's* @return objects stored in each array
+     * @return an array of arrays, each composed of
+     * [0] {@link BwaMemAlignment} object,
+     * [1] expected reference interval,
+     * [2] expected cigar,
+     * [3] expected strandedness,
+     * [4] expected start in assembled contig, 1-based, inclusive
+     * [5] expected end in assembled contig, 1-based, inclusive
+     * [6] expected contig length,
+     * [7] expected {@link AlignedAssembly.AlignmentInterval} object (generated manually with all fields explicitly spell out and given to
+     *                                      {@link AlignedAssembly.AlignmentInterval#AlignmentInterval(SimpleInterval, int, int, Cigar, boolean, int, int, int)}
+     *                                      intended to be used for testing concordance between the two constructors)
+     */
+    @DataProvider(name = "AlignmentIntervalCtorTestForSimpleInversion")
+    private Object[][] createInputsAndExpectedResults_BwaMemAlignmentConstruction() {
+
+        final int[] alignmentStartsOnRef_0Based = {96, 196, 195, 95, 101, 201, 101, 201};
+        final int[] alignmentStartsOnTig_0BasedInclusive = {0, 4, 0, 5, 0, 6, 0, 7};
+        final int[] alignmentEndsOnTig_0BasedExclusive = {4, 8, 5, 10, 6, 12, 7, 14};
+        final int[] seqLen = {8, 8, 10, 10, 12, 12, 14, 14};
+        final int[] mapQualForBwaMemAlgn = {-1, 0, 10, 20, 30, 40, 50, 60};
+        final boolean[] strandedness = {true, false, true, false, false, true, false, true};
+        final String[] cigarStrings = {"4M4S", "4M4H", "5M5S", "5M5H", "6S6M", "6H6M", "7S7M", "7H7M"}; // each different number represent a different contig's pair of chimeric alignments
+        final Cigar[] cigars = Arrays.stream(cigarStrings).map(TextCigarCodec::decode).toArray(Cigar[]::new);
+
+
+        final Object[][] data = new Object[cigars.length][];
+        for(int i=0; i<cigars.length; ++i) {
+            final BwaMemAlignment bwaMemAlignment = new BwaMemAlignment(strandedness[i] ? 0 : SAMFlag.READ_REVERSE_STRAND.intValue(),
+                    0, alignmentStartsOnRef_0Based[i], alignmentStartsOnRef_0Based[i]+cigars[i].getReferenceLength(),
+                    strandedness[i] ? alignmentStartsOnTig_0BasedInclusive[i] : seqLen[i]-alignmentEndsOnTig_0BasedExclusive[i],
+                    strandedness[i] ? alignmentEndsOnTig_0BasedExclusive[i] : seqLen[i]-alignmentStartsOnTig_0BasedInclusive[i],
+                    mapQualForBwaMemAlgn[i], 0, 1, 1, cigarStrings[i],
+                    null, null, 0, Integer.MIN_VALUE, Integer.MAX_VALUE);
+            final SimpleInterval referenceInterval = new SimpleInterval(refNames.get(0), alignmentStartsOnRef_0Based[i]+1, bwaMemAlignment.getRefEnd());
+            final AlignedAssembly.AlignmentInterval alignmentInterval = new AlignedAssembly.AlignmentInterval(referenceInterval, alignmentStartsOnTig_0BasedInclusive[i]+1, alignmentEndsOnTig_0BasedExclusive[i],
+                    strandedness[i] ? cigars[i] : CigarUtils.invertCigar(cigars[i]),
+                    strandedness[i], Math.max(SAMRecord.NO_MAPPING_QUALITY, bwaMemAlignment.getMapQual()), bwaMemAlignment.getNMismatches(), bwaMemAlignment.getAlignerScore());
+            data[i] = new Object[]{bwaMemAlignment, referenceInterval, strandedness[i] ? cigars[i] : CigarUtils.invertCigar(cigars[i]),
+                    strandedness[i], alignmentStartsOnTig_0BasedInclusive[i]+1, alignmentEndsOnTig_0BasedExclusive[i], seqLen[i], mapQualForBwaMemAlgn[i], alignmentInterval};
+        }
+        return data;
+    }
+
+    @Test(dataProvider = "AlignmentIntervalCtorTestForSimpleInversion", groups = "sv")
+    public void testConstructionFromBwaMemAlignment(final BwaMemAlignment bwaMemAlignment, final SimpleInterval expectedReferenceInterval, final Cigar expectedCigar,
+                                                    final boolean expectedIsPositiveStrand, final int expectedStartOnContig_1BasedInclusive, final int expectedEndOnContig_1BasedInclusive,
+                                                    final int expectedContigLength, final int expectedMapQualInBwaMemAlignment, final AlignedAssembly.AlignmentInterval expectedAlignmentInterval) {
+
+        final AlignedAssembly.AlignmentInterval alignmentInterval = new AlignedAssembly.AlignmentInterval(bwaMemAlignment, refNames, expectedContigLength);
+        Assert.assertEquals(alignmentInterval.referenceInterval, expectedReferenceInterval);
+        Assert.assertEquals(alignmentInterval.cigarAlong5to3DirectionOfContig, expectedCigar);
+        Assert.assertEquals(alignmentInterval.forwardStrand, expectedIsPositiveStrand);
+        Assert.assertEquals(alignmentInterval.startInAssembledContig, expectedStartOnContig_1BasedInclusive);
+        Assert.assertEquals(alignmentInterval.endInAssembledContig, expectedEndOnContig_1BasedInclusive);
+        Assert.assertEquals(alignmentInterval.mapQual, Math.max(SAMRecord.NO_MAPPING_QUALITY,expectedMapQualInBwaMemAlignment));
+        Assert.assertEquals(alignmentInterval, expectedAlignmentInterval);
+    }
+
+
+
+    @Test(dataProvider = "AlignmentIntervalCtorTestForSimpleInversion", groups = "sv")
+    public void testConstructionFromSAMRecord(final BwaMemAlignment bwaMemAlignment, final SimpleInterval expectedReferenceInterval, final Cigar expectedCigar,
+                                              final boolean expectedIsPositiveStrand, final int expectedStartOnContig_1BasedInclusive, final int expectedEndOnContig_1BasedInclusive,
+                                              final int expectedContigLength, final int expectedMapQualInBwaMemAlignment, final AlignedAssembly.AlignmentInterval expectedAlignmentInterval) {
+
+        final SAMRecord samRecord = BwaMemAlignmentUtils.applyAlignment("whatever", SVDiscoveryTestDataProvider.makeDummySequence(expectedContigLength, (byte)'A'), null, null, bwaMemAlignment, refNames, hg19Header, false, false);
+        final AlignedAssembly.AlignmentInterval alignmentInterval = new AlignedAssembly.AlignmentInterval(samRecord);
+        Assert.assertEquals(alignmentInterval.referenceInterval, expectedReferenceInterval);
+        Assert.assertEquals(alignmentInterval.cigarAlong5to3DirectionOfContig, expectedCigar);
+        Assert.assertEquals(alignmentInterval.forwardStrand, expectedIsPositiveStrand);
+        Assert.assertEquals(alignmentInterval.startInAssembledContig, expectedStartOnContig_1BasedInclusive);
+        Assert.assertEquals(alignmentInterval.endInAssembledContig, expectedEndOnContig_1BasedInclusive);
+        Assert.assertEquals(alignmentInterval.mapQual, Math.max(SAMRecord.NO_MAPPING_QUALITY,expectedMapQualInBwaMemAlignment));
+        Assert.assertEquals(alignmentInterval, expectedAlignmentInterval);
+    }
 
     /**
      * @see AlignmentIntervalUnitTest#createInputsAndExpectedResults_BwaMemAlignmentConstruction()
@@ -42,6 +131,7 @@ public class AlignedAssemblyUnitTest extends BaseTest {
         final int[] seqLen = {8, 8, 10, 10, 12, 12, 14, 14};
         final int[] mapQual = {0, 1, 10, 20, 30, 40, 50, 60};
         final int[] mismatches = {0, 1, 1, 0, 2, 3, 3, 2};
+        final int as = 10;
         final boolean[] strandedness = {true, false, true, false, false, true, false, true};
         final String[] cigarStrings = {"4M4S", "4M4H", "5M5S", "5M5H", "6S6M", "6H6M", "7S7M", "7H7M"}; // each different number represent a different contig's pair of chimeric alignments
         final Cigar[] cigars = Arrays.stream(cigarStrings).map(TextCigarCodec::decode).toArray(Cigar[]::new);
@@ -63,12 +153,20 @@ public class AlignedAssemblyUnitTest extends BaseTest {
             final SimpleInterval referenceIntervalLeft = new SimpleInterval(refNames.get(0), alignmentStartsOnRef_0Based[2*pair]+1, alignmentStartsOnRef_0Based[2*pair]+cigars[2*pair].getReferenceLength()+1);
             final AlignmentInterval alignmentIntervalLeft = new AlignmentInterval(referenceIntervalLeft, alignmentStartsOnTig_0BasedInclusive[2*pair]+1, alignmentEndsOnTig_0BasedExclusive[2*pair],
                     strandedness[2*pair] ? cigars[2*pair] : CigarUtils.invertCigar(cigars[2*pair]),
+<<<<<<< HEAD
                     strandedness[2*pair], mapQual[2*pair], mismatches[2*pair], 100, false);
+=======
+                    strandedness[2*pair], mapQual[2*pair], mismatches[2*pair], as);
+>>>>>>> 0e3ec13... Now I produced association between contigs and ref vs alt plus qualities of the a association
             alignmentIntervalsForSimpleInversion.add(alignmentIntervalLeft);
             final SimpleInterval referenceIntervalRight = new SimpleInterval(refNames.get(0), alignmentStartsOnRef_0Based[2*pair+1]+1, alignmentStartsOnRef_0Based[2*pair+1]+cigars[2*pair+1].getReferenceLength()+1);
             final AlignmentInterval alignmentIntervalRight = new AlignmentInterval(referenceIntervalRight, alignmentStartsOnTig_0BasedInclusive[2*pair+1]+1, alignmentEndsOnTig_0BasedExclusive[2*pair+1],
                     strandedness[2*pair+1] ? cigars[2*pair+1] : CigarUtils.invertCigar(cigars[2*pair+1]),
+<<<<<<< HEAD
                     strandedness[2*pair+1], mapQual[2*pair+1], mismatches[2*pair+1], 100, false);
+=======
+                    strandedness[2*pair+1], mapQual[2*pair+1], mismatches[2*pair+1], as);
+>>>>>>> 0e3ec13... Now I produced association between contigs and ref vs alt plus qualities of the a association
             alignmentIntervalsForSimpleInversion.add(alignmentIntervalRight);
 
             if (pair == 0) {
